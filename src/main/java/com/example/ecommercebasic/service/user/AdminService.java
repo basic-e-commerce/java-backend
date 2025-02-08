c3526d280954c0776aef0c2a6d1bfa832b9dbbb5
Fatih <fatih_0226@outlook.com>
2025-02-08 15:06:09 +0300

    admin create controlleri eklendi

--- src/main/java/com/example/ecommercebasic/service/user/AdminService.java
+++ src/main/java/com/example/ecommercebasic/service/user/AdminService.java
@@ -1,9 +1,12 @@
 package com.example.ecommercebasic.service.user;
 
 import com.example.ecommercebasic.builder.user.AdminBuilder;
+import com.example.ecommercebasic.config.validation.RegexValidation;
 import com.example.ecommercebasic.dto.user.AdminRequestDto;
 import com.example.ecommercebasic.entity.user.Admin;
+import com.example.ecommercebasic.exception.BadRequestException;
 import com.example.ecommercebasic.exception.NotFoundException;
+import com.example.ecommercebasic.exception.ResourceAlreadyExistException;
 import com.example.ecommercebasic.repository.user.AdminRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,19 +18,34 @@ public class AdminService {
     private final AdminRepository adminRepository;
     private final AdminBuilder adminBuilder;
     private final PasswordEncoder passwordEncoder;
+    private final RegexValidation regexValidation;
 
-    public AdminService(AdminRepository adminRepository, AdminBuilder adminBuilder, PasswordEncoder passwordEncoder) {
+    public AdminService(AdminRepository adminRepository, AdminBuilder adminBuilder, PasswordEncoder passwordEncoder, RegexValidation regexValidation) {
         this.adminRepository = adminRepository;
         this.adminBuilder = adminBuilder;
         this.passwordEncoder = passwordEncoder;
+        this.regexValidation = regexValidation;
     }
 
     public String createAdmin(AdminRequestDto adminRequestDto) {
+
+        if (isAdminExist(adminRequestDto.getUsername()))
+            throw new ResourceAlreadyExistException("Username already exists");
+
+        if (!(regexValidation.isValidEmail(adminRequestDto.getUsername()) && regexValidation.isValidPasswword(adminRequestDto.getPassword())))
+            throw new BadRequestException("Invalid credential");
+
+
         Admin admin = adminBuilder.AdminRequestDtoToAdmin(adminRequestDto);
         admin.setPassword(passwordEncoder.encode(admin.getPassword()));
         adminRepository.save(admin);
         return "Success";
     }
+
+    private boolean isAdminExist(String username) {
+        return adminRepository.existsByUsername(username);
+    }
+
 
     public List<Admin> findAllAdmins() {
         return adminRepository.findAll();
