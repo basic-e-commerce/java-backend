7d63aca6682e7d3eaafc4c60b789bfdda705cf30
Fatih <fatih_0226@outlook.com>
2025-02-15 17:51:38 +0300

    cors ayarı ve application yml değiştirildi

--- src/main/java/com/example/ecommercebasic/config/SecurityConfig.java
+++ src/main/java/com/example/ecommercebasic/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     private CorsConfigurationSource corsConfigurationSource() {
         return request -> {
             CorsConfiguration ccfg = new CorsConfiguration();
-            ccfg.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:5173","https://sandbox-api.iyzipay.com","http://litysofttest1.site"));
+            ccfg.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:5173","https://sandbox-api.iyzipay.com","https://litysofttest1.site"));
             ccfg.setAllowedMethods(Collections.singletonList("*"));
             ccfg.setAllowCredentials(true);
             ccfg.setAllowedHeaders(Collections.singletonList("*"));
